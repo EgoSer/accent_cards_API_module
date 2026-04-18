87292a61819f5dbbc584d308f7403ce6bbc99e30
EgoSer <vobschixchertax@gmail.com>
2026-04-18 19:28:57 +0700

    move to core logic

---
+++ src/core/postgres_orm.py
@@ -0,0 +1,13 @@
+from collections.abc import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from src.core.postgres_config import pg_settings
+
+engine = create_async_engine(pg_settings.database_url)
+async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession]:
+    async with async_session_maker() as session:
+        yield session
