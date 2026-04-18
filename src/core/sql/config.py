1a1a0abfaa6e4ad38dc64383445b4c9f31d12385
EgoSer <vobschixchertax@gmail.com>
2026-04-18 23:21:35 +0700

    Update Base model

--- src/core/sql/config.py
+++ src/core/sql/config.py
@@ -1,6 +1,7 @@
 import os
 from functools import cached_property
 
+from loguru import logger
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -10,22 +11,30 @@ def coalesce(value, default):
 
 class PGSettings(BaseSettings):
     USER: str | None
-    PASSWORD: int | None
+    PASSWORD: str | None
     HOST: str | None
-    PORT: int | None
+    PORT_POOLER: int | None
+    PORT_DATABASE: int | None
     DATABASE_NAME: str | None
 
     model_config = SettingsConfigDict()
 
     @cached_property
     def database_url(self):
-        return f"postgresql+asyncpg://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE_NAME}"
+        return f"postgresql+asyncpg://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT_DATABASE}/{self.DATABASE_NAME}"
+
+    @cached_property
+    def database_pooler_url(self):
+        return f"postgresql+asyncpg://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT_POOLER}/{self.DATABASE_NAME}"
 
 
 pg_settings = PGSettings(
     USER=coalesce(os.getenv("POSTGRES_USER"), ""),
-    PASSWORD=int(coalesce(os.getenv("POSTGRES_PASSWORD"), "")),
+    PASSWORD=coalesce(os.getenv("POSTGRES_PASSWORD"), ""),
     HOST=coalesce(os.getenv("POSTGRES_HOST"), ""),
-    PORT=int(coalesce(os.getenv("POSTGRES_PORT"), "")),
+    PORT_POOLER=int(coalesce(os.getenv("PGBOUNCER_PORT"), "")),
+    PORT_DATABASE=int(coalesce(os.getenv("POSTGRES_PORT"), "")),
     DATABASE_NAME=coalesce(os.getenv("POSTGRES_DB"), ""),
 )
+
+logger.debug(f"Created settings object: {pg_settings}")
