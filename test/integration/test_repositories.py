eea0218a81e7caeaacb2e07c0530ab16eb89ed05
EgoSer <vobschixchertax@gmail.com>
2026-04-20 22:15:59 +0700

    Create class fixture to setup single card for CRUD tests

--- test/integration/test_repositories.py
+++ test/integration/test_repositories.py
@@ -1,4 +1,5 @@
 import pytest
+import pytest_asyncio
 from loguru import logger
 from sqlalchemy import select
 
@@ -8,8 +9,8 @@ from src.modules.accent_cards.models import Card
 class TestAccentCardCRUD:
     """Test CRUD operations on Accent card model"""
 
-    @pytest.mark.asyncio
-    async def test_create_card(self, db_class_session):
+    @pytest_asyncio.fixture(scope="class", loop_scope="class", autouse=True)
+    async def setup_card(self, db_class_session):
         new_card = Card(word="торты", accent=2)
 
         db_class_session.add(new_card)
@@ -17,7 +18,20 @@ class TestAccentCardCRUD:
         await db_class_session.refresh(new_card)
 
         assert new_card.id is not None
-        self.card_id = new_card.id
+        TestAccentCardCRUD.card_id = new_card.id
+        logger.info(f"setup_card fixture: Created card: {new_card}")
+        yield
+
+    @pytest.mark.asyncio
+    async def test_create_card(self, db_class_session):
+        new_card = Card(word="машины", accent=4)
+
+        db_class_session.add(new_card)
+        await db_class_session.flush()
+        await db_class_session.refresh(new_card)
+
+        assert new_card.id is not None
+        assert new_card.id != TestAccentCardCRUD.card_id
         logger.info(f"CREATE test: Created card: {new_card}")
 
     @pytest.mark.asyncio
@@ -30,7 +44,7 @@ class TestAccentCardCRUD:
         assert card is not None
 
         logger.info(f"READ test: Found card: {card}")
-        assert card.id == self.card_id
+        assert card.id == TestAccentCardCRUD.card_id
         assert card.word == "торты"
         assert card.accent == 2
 
@@ -50,7 +64,7 @@ class TestAccentCardCRUD:
         await db_class_session.refresh(card)
 
         # Read again
-        query = select(Card).where(Card.id == self.card_id)
+        query = select(Card).where(Card.id == TestAccentCardCRUD.card_id)
         card = (await db_class_session.execute(query)).scalar_one_or_none()
 
         assert card is not None
@@ -62,7 +76,7 @@ class TestAccentCardCRUD:
     async def test_delete_card(self, db_class_session):
         assert hasattr(self, "card_id")
 
-        query = select(Card).where(Card.id == self.card_id)
+        query = select(Card).where(Card.id == TestAccentCardCRUD.card_id)
         card = (await db_class_session.execute(query)).scalar_one_or_none()
 
         assert card is not None
@@ -71,7 +85,7 @@ class TestAccentCardCRUD:
         db_class_session.delete(card)
         await db_class_session.flush()
 
-        query = select(Card).where(Card.id == self.card_id)
+        query = select(Card).where(Card.id == TestAccentCardCRUD.card_id)
         card = (await db_class_session.execute(query)).scalar_one_or_none()
 
         assert card is None
