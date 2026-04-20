61267b85fe92b02c5c28d63e8f21c5dd6d989c65
EgoSer <vobschixchertax@gmail.com>
2026-04-20 22:06:22 +0700

    Update logger configuration for tests

--- logger_configuration.py
+++ logger_configuration.py
@@ -5,7 +5,15 @@ from pathlib import Path
 from loguru import logger
 
 
+def testing_log_filter(record):
+    if os.getenv("STAGE") == "TEST":
+        return False
+    return True
+
+
 def database_log_filter(record):
+    if os.getenv("STAGE") == "TEST":
+        return False
     if not hasattr(record, "name"):
         return False
 
@@ -39,7 +47,14 @@ def set_logger():
     log_level = "DEBUG" if os.getenv("STAGE") == "DEBUG" else "INFO"
 
     # set logging file for app
-    logger.add(log_folder / "app.log", rotation="10 MB", retention="7 days", level=log_level, enqueue=True)
+    logger.add(
+        log_folder / "app.log",
+        filter=testing_log_filter,
+        rotation="10 MB",
+        retention="7 days",
+        level=log_level,
+        enqueue=True,
+    )
 
     # set logging file for postgres interactions
     logger.add(
