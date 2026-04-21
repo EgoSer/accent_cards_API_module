c917fd7bd35328236d49555595f868ecb7f1a3f3
EgoSer <vobschixchertax@gmail.com>
2026-04-22 01:30:43 +0700

    Rollback

--- test/integration/core/test_endpoints.py
+++ test/integration/core/test_endpoints.py
@@ -1,8 +1,6 @@
 from fastapi.testclient import TestClient
 
-from src.modules.accent_cards.meta import module_name as accent_module_name
-from src.modules.accent_cards.meta import prefix as accent_prefix
-from src.modules.accent_cards.meta import version as accent_version
+import src.modules.accent_cards as accent_cards
 from src.server import app
 
 client = TestClient(app)
@@ -16,7 +14,7 @@ def test_root_endpoint():
 
 
 def test_module_endpoint():
-    response = client.get(accent_prefix)
+    response = client.get(accent_cards.prefix)
 
     assert response.status_code == 200
-    assert response.json() == {"module": accent_module_name, "version": accent_version}
+    assert response.json() == {"module": accent_cards.module_name, "version": accent_cards.version}
