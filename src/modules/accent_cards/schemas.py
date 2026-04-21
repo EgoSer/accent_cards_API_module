7a06e391dd158c72a4470e6951582807963ad39a
EgoSer <vobschixchertax@gmail.com>
2026-04-21 20:31:51 +0700

    Add word field filtration logic

--- src/modules/accent_cards/schemas.py
+++ src/modules/accent_cards/schemas.py
@@ -1,12 +1,25 @@
 import uuid
 from typing import Annotated, Self
 
-from pydantic import BaseModel, ConfigDict, Field, model_validator
+from pydantic import BaseModel, ConfigDict, Field, field_validator, model_validator
+
+allowed_characters = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 
 class CardSchema(BaseModel):
-    word: Annotated[str, Field(max_length=38, description="A word in lowercase without any trailing symbols")]
-    accent: Annotated[int, Field(description="A number of letter starting with 0 which points to true accented letter")]
+    word: Annotated[
+        str, Field(min_length=1, max_length=38, description="A word in lowercase without any trailing symbols")
+    ]
+    accent: Annotated[
+        int, Field(gt=-1, description="A number of letter starting with 0 which points to true accented letter")
+    ]
+
+    @field_validator("word", mode="before")
+    def word_field_validator(cls, value) -> bool:
+        if set(value).issubset(allowed_characters):
+            return True
+        else:
+            raise ValueError("Word contains unallowed characters!")
 
     @model_validator(mode="after")
     def word_validator(self) -> Self:
