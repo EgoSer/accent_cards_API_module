daa11d2e76c66bf7d745ab77258854db3b9f4e8a
EgoSer <vobschixchertax@gmail.com>
2026-04-22 00:30:26 +0700

    Add dependency overrides

--- test/integration/modules/test_endpoints_accent_cards.py
+++ test/integration/modules/test_endpoints_accent_cards.py
@@ -3,6 +3,7 @@ import json
 import pytest
 from fastapi.testclient import TestClient
 
+from src.core.sql.dependencies import get_async_session
 from src.modules.accent_cards.meta import prefix
 from src.modules.accent_cards.models import Card
 from src.server import app
@@ -15,27 +16,33 @@ def accent_keywords():
     return [("торты", 1), ("туфля", 1), ("штаны", 4), ("машина", 3), ("ёжики", 0), ("двухядерный", 4)]
 
 
-def test_get_cards_endpoint_no_cards():
+def test_get_cards_endpoint_no_cards(test_async_session_maker):
+    app.dependency_overrides[get_async_session] = test_async_session_maker
     amount = 10
     response = client.get(f"{prefix}/get_cards?amount={amount}")
+    app.dependency_overrides.clear()
 
     assert response.status_code == 200
     assert response.json() == json.dumps({"cards": []})
 
 
 @pytest.mark.asyncio
-async def test_create_cards(db_session, accent_keywords):
+async def test_create_cards(db_session, accent_keywords, test_async_session_maker):
+    app.dependency_overrides[get_async_session] = test_async_session_maker
     cards = [Card(word=word, accent=accent) for word, accent in accent_keywords]
 
     db_session.add(cards)
     await db_session.commit()
 
     assert cards is not None
+    app.dependency_overrides.clear()
 
 
-def test_get_cards_endpoint_less_cards(accent_keywords):
+def test_get_cards_endpoint_less_cards(accent_keywords, test_async_session_maker):
+    app.dependency_overrides[get_async_session] = test_async_session_maker
     amount = 10
     response = client.get(f"{prefix}/get_cards?amount={amount}")
+    app.dependency_overrides.clear()
 
     assert response.status_code == 200
 
@@ -45,9 +52,11 @@ def test_get_cards_endpoint_less_cards(accent_keywords):
         assert word in result
 
 
-def test_get_cards_endpoint_more_cards(accent_keywords):
+def test_get_cards_endpoint_more_cards(accent_keywords, test_async_session_maker):
+    app.dependency_overrides[get_async_session] = test_async_session_maker
     amount = 2
     response = client.get(f"{prefix}/get_cards?amount={amount}")
+    app.dependency_overrides.clear()
 
     assert response.status_code == 200
 
