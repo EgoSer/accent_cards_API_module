8554993b4e65400f640e41cfa39e516b739a9737
EgoSer <vobschixchertax@gmail.com>
2026-04-22 00:44:52 +0700

    Add redis test for decoding responses

--- test/integration/core/test_redis.py
+++ test/integration/core/test_redis.py
@@ -7,6 +7,12 @@ async def test_redis_write(redis_session):
 
 
 @pytest.mark.asyncio
+async def test_redis_decode(redis_session):
+    value = await redis_session.get("Test:key")
+    assert isinstance(value, str)
+
+
+@pytest.mark.asyncio
 async def test_redis_read(redis_session):
     value = await redis_session.get("Test:key")
     assert value == "Test value"
