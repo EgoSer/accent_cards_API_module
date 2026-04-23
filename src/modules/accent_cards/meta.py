87195cae2a78bdb9ceb27ed016b1ee8ab3960df2
EgoSer <vobschixchertax@gmail.com>
2026-04-23 20:44:01 +0700

    Update description

--- src/modules/accent_cards/meta.py
+++ src/modules/accent_cards/meta.py
@@ -7,8 +7,7 @@ module_tags: list[str | Enum] | None  # required for automatic importing
 module_name = "Accent cards"  # The name of your module
 version = "0.2.1-beta"  # Actual version of your module
 description = (
-    "Returns requested amount of accent cards in format "
-    "{'cards': ['word': 'слово', 'accent': '2', 'id': '[[ uuid of word ]]']}"
-)  # description of your module
+    "Returns requested amount of accent cards (or less if database have not enough)"  # description of your module
+)
 prefix = "/cards/accent"  # prefix is used by fastapi Router class to define how to access your module
 module_tags = ["accent"]
