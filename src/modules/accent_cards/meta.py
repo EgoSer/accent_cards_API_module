9cb57df441086615e181dfec7eba5650cf50fe59
EgoSer <vobschixchertax@gmail.com>
2026-04-23 17:21:10 +0700

    Update version of module

--- src/modules/accent_cards/meta.py
+++ src/modules/accent_cards/meta.py
@@ -5,6 +5,6 @@ module_tags: list[str | Enum] | None  # required for automatic importing
 # Metadata used by module
 
 module_name = "Accent cards"  # The name of your module
-version = "0.1a"  # Actual version of your module
+version = "0.2.1-alpha"  # Actual version of your module
 prefix = "/cards/accent"  # prefix is used by fastapi Router class to define how to access your module
 module_tags = ["accent"]
